659c0cc93e0c981ed8a82ab9c0f661180a0be34f
Juliana Alves <analistajferreiraalves@gmail.com>
2024-10-01 15:00:30 -0300

    add commit validações RestAssured

--- src/main/java/julianaalves/rest/OlaMundo.java
+++ src/main/java/julianaalves/rest/OlaMundo.java
@@ -8,6 +8,7 @@ import io.restassured.response.ValidatableResponse;
 public class OlaMundo {
 
 	public static void main(String[] args) {
+		//Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me:80/ola");
 		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
 		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
 		System.out.println(response.statusCode() == 200);
--- src/main/java/julianaalves/rest/OlaMundoTest.java
+++ src/main/java/julianaalves/rest/OlaMundoTest.java
@@ -18,44 +18,42 @@ import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
 public class OlaMundoTest {
+	
 	@Test
-
 	public void testOlaMundo() {
 		Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
 		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
 		Assert.assertTrue(response.statusCode() == 200);
-		Assert.assertTrue("O status code deveria ser 200 ", response.statusCode() == 200);
+		Assert.assertTrue("O status code deveria ser 201",response.statusCode() == 200);
 		Assert.assertEquals(200, response.statusCode());
-
+				
 		ValidatableResponse validacao = response.then();
 		validacao.statusCode(200);
-
 	}
 	
-// 	Montando a estrutura de Teste	
-
 	@Test
 	public void devoConhecerOutrasFormasRestAssured() {
-//		1ª opção: Forma mais prolixa de escrever a requisição de teste
 		Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
 		ValidatableResponse validacao = response.then();
 		validacao.statusCode(200);
-
-//		2ª opção: Forma mais simplificada de escrever a requisição de teste
+		
 		get("https://restapi.wcaquino.me/ola").then().statusCode(200);
-
-//		3ª opção: Forma ideal de escrever a requisição de teste (Given, When, Then)
-		given() //(Pré condições) Dado que, ao fazer uma requisição, 
-		.when() //(Ação de Fato, o que será realizado) Quando eu fizer uma requisição para a página https://restapi.wcaquino.me/ola
+		
+//		Given(É uma pré condição que eu queira aplicar)
+//		when (É a ação de fato -> Quando eu fizer a requisição) 
+//		Then (São as asseritivas -> O que iresmo fazer (Testar), podemos colocar quantas verificações quizermos)
+		
+		given()
+		.when()
 			.get("https://restapi.wcaquino.me/ola")
-		.then() //(São as verificações, as Assertivas) Então o meu status code deverá ser 200
-//			.assertThat() Então verifique que 
-			.statusCode(200); // O status code é 200
+		.then()
+//			.assertThat()
+			.statusCode(200);
+		
 	}
 	
-//	Entendendo Hamcrest
 	@Test
-	public void devoConhecerMatcherHamcrest() {
+	public void devoConhecerMatchersHamcrest() {
 		Assert.assertThat("Maria", Matchers.is("Maria"));
 		Assert.assertThat(128, Matchers.is(128));
 		Assert.assertThat(128, Matchers.isA(Integer.class));
@@ -63,18 +61,29 @@ public class OlaMundoTest {
 		Assert.assertThat(128d, Matchers.greaterThan(120d));
 		Assert.assertThat(128d, Matchers.lessThan(130d));
 		
-		List<Integer> impares= Arrays.asList(1,3,5,7,9);
-		assertThat(impares,hasSize(5));
+		List<Integer> impares = Arrays.asList(1,3,5,7,9);
+		assertThat(impares, hasSize(5));
 		assertThat(impares, contains(1,3,5,7,9));
-		assertThat(impares, containsInAnyOrder(1,3,5,7,9));
-		assertThat(impares, hasItem(5));
-		assertThat(impares, hasItems(5, 7));
+		assertThat(impares, containsInAnyOrder(1,3,5,9,7));
+		assertThat(impares, hasItem(1));
+		assertThat(impares, hasItems(1,5));
 		
 		assertThat("Maria", is(not("João")));
 		assertThat("Maria", not("João"));
-		assertThat("Bia", anyOf(is("Maria"), is("Bia")));
+		assertThat("Maria",anyOf(is("Maria"), is("Joaquina")));
 		assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
-		
 	}
-
+	
+	@Test
+	public void devoValidarBody() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/ola")
+		.then()
+			.statusCode(200)
+			.body(is("Ola Mundo!"))
+			.body(containsString("Mundo"))
+			.body(is(not(nullValue())));
+	}
+	
 }
